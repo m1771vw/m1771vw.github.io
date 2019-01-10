29c3b73a8055751e127f124c3802ef9955af64dd
William Yang <williamyang93@gmail.com>
2019-01-09 23:21:08 -0800

    Updates

---
+++ precache-manifest.7dffb4ed3f27d3892bed468e64c7d2dc.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "104dbb227da85c1a730e109cf881df87",
+    "url": "/m1771vw.github.io/static/media/BouncingBlockResized.104dbb22.png"
+  },
+  {
+    "revision": "247041802bd0444999d4",
+    "url": "/m1771vw.github.io/static/css/main.d9a6e8fc.chunk.css"
+  },
+  {
+    "revision": "60c03632262e3a9f6dbf",
+    "url": "/m1771vw.github.io/static/js/1.60c03632.chunk.js"
+  },
+  {
+    "revision": "207436204f94d64c239c",
+    "url": "/m1771vw.github.io/static/js/runtime~main.20743620.js"
+  },
+  {
+    "revision": "8d3d5f08b0b3446aecc61674f962e45f",
+    "url": "/m1771vw.github.io/static/media/StudyBuddiesResize.8d3d5f08.png"
+  },
+  {
+    "revision": "dd806ded2d08567e1fb4d6c22cba0869",
+    "url": "/m1771vw.github.io/static/media/StudyBuddiesResizeMobile.dd806ded.png"
+  },
+  {
+    "revision": "247041802bd0444999d4",
+    "url": "/m1771vw.github.io/static/js/main.24704180.chunk.js"
+  },
+  {
+    "revision": "861666454cf8905853ca4e85fd49ba50",
+    "url": "/m1771vw.github.io/static/media/JobTrackerResized.86166645.png"
+  },
+  {
+    "revision": "37476c4516ff5c777f0392764b2d304c",
+    "url": "/m1771vw.github.io/static/media/PUGResized.37476c45.png"
+  },
+  {
+    "revision": "3c31a11adc9124006c23a2565df9ccb5",
+    "url": "/m1771vw.github.io/static/media/rnge.3c31a11a.png"
+  },
+  {
+    "revision": "a9f04de73952fd4a5b95ddfe426e0276",
+    "url": "/m1771vw.github.io/static/media/GraphQLNodeMongoResized.a9f04de7.png"
+  },
+  {
+    "revision": "40d46b490d0c5a95e7f0565d3f06cb4c",
+    "url": "/m1771vw.github.io/static/media/garibaldi_lake_wallpaper.40d46b49.jpg"
+  },
+  {
+    "revision": "43fd1658b1e781c7909772886b4f3843",
+    "url": "/m1771vw.github.io/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.12e329b760e941d48fcb79d7df092591.js"
+  "/m1771vw.github.io/precache-manifest.7dffb4ed3f27d3892bed468e64c7d2dc.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/m1771vw.github.io/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
