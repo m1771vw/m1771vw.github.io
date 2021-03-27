ac73f8816138955c4385222d3311dc233fb29728
William Yang <williamyang93@gmail.com>
2021-03-27 02:44:15 -0700

    New resume added

---
+++ precache-manifest.4b4f8e946de8b72b2754313d93c5d76f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "bab4d0c49eab0136239b",
+    "url": "/william-portfolio/static/css/main.b17a7030.chunk.css"
+  },
+  {
+    "revision": "bab4d0c49eab0136239b",
+    "url": "/william-portfolio/static/js/main.bab4d0c4.chunk.js"
+  },
+  {
+    "revision": "5d8524a66bac086cd853",
+    "url": "/william-portfolio/static/js/1.5d8524a6.chunk.js"
+  },
+  {
+    "revision": "aa0fd5109e16cef60e20",
+    "url": "/william-portfolio/static/js/runtime~main.aa0fd510.js"
+  },
+  {
+    "revision": "8d3d5f08b0b3446aecc61674f962e45f",
+    "url": "/william-portfolio/static/media/StudyBuddiesResize.8d3d5f08.png"
+  },
+  {
+    "revision": "dd806ded2d08567e1fb4d6c22cba0869",
+    "url": "/william-portfolio/static/media/StudyBuddiesResizeMobile.dd806ded.png"
+  },
+  {
+    "revision": "104dbb227da85c1a730e109cf881df87",
+    "url": "/william-portfolio/static/media/BouncingBlockResized.104dbb22.png"
+  },
+  {
+    "revision": "861666454cf8905853ca4e85fd49ba50",
+    "url": "/william-portfolio/static/media/JobTrackerResized.86166645.png"
+  },
+  {
+    "revision": "37476c4516ff5c777f0392764b2d304c",
+    "url": "/william-portfolio/static/media/PUGResized.37476c45.png"
+  },
+  {
+    "revision": "3c31a11adc9124006c23a2565df9ccb5",
+    "url": "/william-portfolio/static/media/rnge.3c31a11a.png"
+  },
+  {
+    "revision": "daac8eeeb5374072b684b5df35da869d",
+    "url": "/william-portfolio/static/media/William_Yang_Resume.daac8eee.pdf"
+  },
+  {
+    "revision": "a9f04de73952fd4a5b95ddfe426e0276",
+    "url": "/william-portfolio/static/media/GraphQLNodeMongoResized.a9f04de7.png"
+  },
+  {
+    "revision": "40d46b490d0c5a95e7f0565d3f06cb4c",
+    "url": "/william-portfolio/static/media/garibaldi_lake_wallpaper.40d46b49.jpg"
+  },
+  {
+    "revision": "7176d8ae6c6c6eed1a129e1c5a074738",
+    "url": "/william-portfolio/index.html"
+  }
+];
